afaa103008993bde25b790c4a84f5fb482f7f355
Ibrahim Dharhan <abrahamdharhan2255@gmail.com>
2024-06-21 20:11:22 -0700

    Setting up infra

---
+++ util.py
@@ -0,0 +1,16 @@
+#### Calculate the metrics RMSE and MAPE ####
+def calculate_rmse(y_true, y_pred):
+    """
+    Calculate the Root Mean Squared Error (RMSE)
+    """
+    rmse = np.sqrt(np.mean((y_true - y_pred) ** 2))
+    return rmse
+
+
+def calculate_mape(y_true, y_pred):
+    """
+    Calculate the Mean Absolute Percentage Error (MAPE) %
+    """
+    y_pred, y_true = np.array(y_pred), np.array(y_true)
+    mape = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+    return mape
